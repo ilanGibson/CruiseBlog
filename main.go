2fa942659428ae0b5347087aa876de511e6cea29
Ilan Gibson <ilanjgibson@gmail.com>
2026-01-27 11:29:52 -0700

    write cookies to response

--- main.go
+++ main.go
@@ -11,14 +11,36 @@ type connectionCounter struct {
 	sync.Mutex
 }
 
+type user struct {
+}
+
+func (c *connectionCounter) addConnection() {
+	c.Mutex.Lock()
+	defer c.Mutex.Unlock()
+	c.connections++
+}
+
+func (c *connectionCounter) removeConnection() {
+	c.Mutex.Lock()
+	defer c.Mutex.Unlock()
+	c.connections--
+}
+
 func hello(w http.ResponseWriter, req *http.Request) {
+	cookie := new(http.Cookie)
+	cookie.Name = "username"
+	cookie.Value = "dilliontomphson"
+	fmt.Printf("%+v\n", cookie)
+	http.SetCookie(w, cookie)
 	fmt.Fprint(w, "hello\n")
 }
 
-func main() {
-	c := &connectionCounter{connections: 0}
+var c connectionCounter
 
+func main() {
+	// msgs := make(map[string]string)
 	http.HandleFunc("/hel", hello)
 
+	fmt.Println("running server...")
 	http.ListenAndServe(":8090", nil)
 }
