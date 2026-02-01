8792b33b0920c5f5fb155c1c269717765ccc3133
Ilan Gibson <ilanjgibson@gmail.com>
2026-02-01 15:11:14 -0500

    move helper func to utils.go

---
+++ utils.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"slices"
+	"strings"
+)
+
+func CleanPost(content string) bool {
+	badWords := []string{"fuck", "shit", "ass", "bitch", "cunt", "whore"}
+	// maxPostLen
+	content = "fuck"
+	//
+	// againstLangPolicyFlag = check if content contains badWords
+	if slices.Contains(badWords, content) {
+		fmt.Println("here")
+		return false
+	}
+	// againstLengthPolicyFlag = check if content within maxPostLen
+	return true
+}
+
+func GetRandValue() string {
+	var characters = []rune("ABCDEFG0123456789")
+	var sb strings.Builder
+
+	for range 8 {
+		randomIndex := rand.Intn(len(characters))
+		randomChar := characters[randomIndex]
+		sb.WriteRune(randomChar)
+	}
+
+	return sb.String()
+}
