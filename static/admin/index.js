6612f8e86343ea7c88f9a064f6915c0c4f06beb4
Ilan Gibson <ilanjgibson@gmail.com>
2026-02-25 19:29:31 -0700

    rough implementation of front end admin page

--- static/admin/index.js
+++ static/admin/index.js
@@ -1,5 +1,50 @@
 const totalUsers = document.getElementById("totalUsers");
 const totalPosts = document.getElementById("totalPosts");
+const contentContainer = document.getElementById("content");
+const postContainer = document.getElementById("postContainer");
+const editPostBtn = document.getElementById("editPost-btn");
+
+let activeSection;
+let activeSectionUsername;
+let activeSectionTimeDate;
+let newContentForUpdate;
+
+function listPosts() {
+  fetch('/api/posts')
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      if (data != null) {
+        postContainer.replaceChildren();
+        for (let i = 0; i < data.length; i++) {
+          newPost(data[i].date, data[i].username, data[i].content);
+        }
+      }
+    })
+    .catch(err => {
+      console.error("list posts admin err", err)
+    })
+}
+
+function newPost(dateOfPost, postUsername, postContent) {
+  const newPost = document.createElement('section');
+  newPost.classList.add('blog-post');
+
+  const date = document.createElement('p');
+  date.classList.add('date');
+  date.textContent = dateOfPost;
+
+  const content = document.createElement('p');
+  content.classList.add('content');
+  content.textContent = `${postUsername}: ${postContent}`
+
+  newPost.appendChild(date);
+  newPost.appendChild(content);
+
+  postContainer.prepend(newPost);
+}
+
 
 function connect() {
   const sseTotalUsers = new EventSource('/admin/sseEvents');
@@ -19,4 +64,55 @@ function connect() {
   };
 }
 
-connect()
+if (window.location.pathname === "/admin/") {
+  connect();
+  listPosts();
+}
+
+postContainer.addEventListener("click", (e) => {
+  activeSection = e.target.closest("section");
+  if (!activeSection) return;
+
+  postContainer.querySelectorAll("section")
+    .forEach(s => {
+      s.classList.remove("active")
+      });
+
+  activeSection.classList.add("active");
+  activeSectionTimeDate = activeSection.children[0].textContent;
+  activeSectionUsername = activeSection.children[1].textContent.slice(0,8);
+  contentContainer.value = activeSection.children[1].textContent.slice(10);
+})
+
+editPostBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  newContentForUpdate = contentContainer.value;
+
+  fetch('/admin/post/update', {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      date: activeSectionTimeDate,
+      username: activeSectionUsername,
+      content: newContentForUpdate
+    })
+  })
+  .then(res => {
+      if (!res.ok) {
+        throw new Error(`http error! status ${res.status}`);
+      }
+      return res;
+    })
+  .then(data => {
+      const writeContent = `${activeSectionUsername}: ${contentContainer.value}`
+      activeSection.children[1].textContent = writeContent;
+      activeSection.classList.remove("active");
+      contentContainer.value = "";
+
+    })
+  .catch(error => {
+      console.error("error updating post:", error);
+    });
+});
